1bd357b80d12116edbad79dc7bd5d04dd9998c00
Oldrich Strachota <oldrich@strachota.net>
2020-06-04 00:54:32 +0200

    oprava tabu

--- pki/extract-ssl-cert-from-pfx.sh
+++ pki/extract-ssl-cert-from-pfx.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# extract-ssl-cert-from-pfx.sh; verze 2020-06-03; strachotao 
+# extract-ssl-cert-from-pfx.sh; verze 2020-06-04; strachotao 
 #
 #  v poli pfx je: ["soubor-certifikatu-a-klice.pfx"]="hesloklice"
 
 declare -A pfx=(
-        ["strachota.eu.pfx"]="heslo"
-        ["strachota.net.pfx"]="heslo"
+	["strachota.eu.pfx"]="heslo"
+	["strachota.net.pfx"]="heslo"
 )
 
 DELIMITER=$(printf '%46s\n' | tr ' ' =)
@@ -15,11 +15,11 @@ debug=true
 while [[ $# -gt 0 ]]; do
 	param="$1"
 	shift;
-    case $param in
+	case $param in
 		-f|--intmfil)	intmfil="$1";shift;;
 		-p|--process)	debug=false;;
 		*) echo "Spatny parametr!";;
-    esac
+    	esac
 done
 
 echo "pouziti: $0 [-p] [--process] [-f] [--intmfil]"
@@ -34,63 +34,55 @@ fi
 
 for item in "${!pfx[@]}";
 do
-		echo "Start ${item}"
-        if [ ! -f ${item} ]; then
-				echo -e "$(tput setaf 1)Zdrojovy soubor ${item} nelze precist!$(tput sgr0)\n${DELIMITER}"
-                continue
-        fi
-        openssl pkcs12 -in ${item} -noout -passin pass:"${pfx["$item"]}"
-        if [ $? -eq 1 ]; then
-                echo -e "$(tput setaf 1)${item} nemame spravne heslo!$(tput sgr0)\n${DELIMITER}"
-                continue
-        fi
-        echo -e "$(tput setaf 2)${item} heslo OK$(tput sgr0)"
-        if [[ $debug == true ]]; then
-                echo -e "$DELIMITER"
-                continue
-        fi
-
-		echo "Exportuji..."
-
-        filen="$item"
-        item=$(sed 's/\.pfx//' <<< ${item})
-
-        openssl pkcs12 -in ${filen} -clcerts -nokeys -out ${item}.cer -passin pass:"${pfx["$filen"]}"
-        openssl pkcs12 -in ${filen} -nocerts -nodes  -out ${item}.enc.key -passin pass:"${pfx["$filen"]}"
-        openssl rsa -in ${item}.enc.key -out ${item}.key
-        rm -f ${item}.enc.key
-
+	echo "Start ${item}"
+	if [ ! -f ${item} ]; then
+		echo -e "$(tput setaf 1)Zdrojovy soubor ${item} nelze precist!$(tput sgr0)\n${DELIMITER}"
+		continue
+	fi
+	openssl pkcs12 -in ${item} -noout -passin pass:"${pfx["$item"]}"
+	if [ $? -eq 1 ]; then
+		echo -e "$(tput setaf 1)${item} nemame spravne heslo!$(tput sgr0)\n${DELIMITER}"
+		continue
+	fi
+	echo -e "$(tput setaf 2)${item} heslo OK$(tput sgr0)"
+	if [[ $debug == true ]]; then
+		echo -e "$DELIMITER"
+		continue
+	fi
+	echo "Exportuji..."
+	filen="$item"
+	item=$(sed 's/\.pfx//' <<< ${item})
+	openssl pkcs12 -in ${filen} -clcerts -nokeys -out ${item}.cer -passin pass:"${pfx["$filen"]}"
+	openssl pkcs12 -in ${filen} -nocerts -nodes  -out ${item}.enc.key -passin pass:"${pfx["$filen"]}"
+	openssl rsa -in ${item}.enc.key -out ${item}.key
+	rm -f ${item}.enc.key
 	if [[ -n ${intmfil} ]]; then
 		cat ${intmfil} >> ${item}.cer
 	fi
+	read -p "Zkopirovat klice ${item} do /etc/pki/tls/certs|private...? (y/n)" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		if [[ -f /etc/pki/tls/certs/${item}.cer ]]; then
+			cp /etc/pki/tls/certs/${item}.cer /etc/pki/tls/certs/${item}.cer.bck$(date +%Y%m%d)
+		fi
 
-        read -p "Zkopirovat klice ${item} do /etc/pki/tls/certs|private...? (y/n)" -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
+		if [[ -f /etc/pki/tls/private/${item}.key ]]; then
+			cp /etc/pki/tls/private/${item}.key /etc/pki/tls/private/${item}.key.bck$(date +%Y%m%d)
+		fi
 
-                if [[ -f /etc/pki/tls/certs/${item}.cer ]]; then
-                        cp /etc/pki/tls/certs/${item}.cer /etc/pki/tls/certs/${item}.cer.bck$(date +%Y%m%d)
-                fi
+		cp -v ${item}.cer /etc/pki/tls/certs/${item}.cer
+		cp -v ${item}.key /etc/pki/tls/private/${item}.key
 
-                if [[ -f /etc/pki/tls/private/${item}.key ]]; then
-                        cp /etc/pki/tls/private/${item}.key /etc/pki/tls/private/${item}.key.bck$(date +%Y%m%d)
-                fi
-
-                cp -v ${item}.cer /etc/pki/tls/certs/${item}.cer
-                cp -v ${item}.key /etc/pki/tls/private/${item}.key
-
-        fi
-        echo
-        echo "konfigurace pro apache:"
-        echo "SSLCertificateFile /etc/pki/tls/certs/${item}.cer"
-        echo "SSLCertificateKeyFile /etc/pki/tls/private/${item}.key"
-        echo "SSLCACertificateFile /etc/pki/tls/certs/digicert-thawte-int.cer"
-        echo
-        echo "konfigurace nginx:"
-        echo "ssl_certificate /etc/nginx/pki/${item}.cer;"
-        echo "ssl_certificate_key /etc/nginx/pki/${item}.key;"
-        echo
-        echo -e "Hotovo!\n${DELIMITER}"
-
+	fi
+	echo
+	echo "konfigurace pro apache:"
+	echo "SSLCertificateFile /etc/pki/tls/certs/${item}.cer"
+	echo "SSLCertificateKeyFile /etc/pki/tls/private/${item}.key"
+	echo "SSLCACertificateFile /etc/pki/tls/certs/digicert-thawte-int.cer"
+	echo
+	echo "konfigurace nginx:"
+	echo "ssl_certificate /etc/nginx/pki/${item}.cer;"
+	echo "ssl_certificate_key /etc/nginx/pki/${item}.key;"
+	echo
+	echo -e "Hotovo!\n${DELIMITER}"
 done
