b5842316d74b59a83111f0fe3085b57f5852034d
Oldrich Strachota <oldrich@strachota.net>
2020-09-11 17:39:15 +0200

    upload

---
+++ CSES/WeirdAlgorithm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#https://cses.fi/problemset/task/1068
+result="$1 "
+n=$1
+until [[ $n -eq 1 ]]; do
+	if [[ $((n%2)) -eq 0 ]]; then
+		n=$((n/2))
+	else
+		n=$((n*3+1))
+	fi
+	result="$result $n "
+done
+
+echo $result
